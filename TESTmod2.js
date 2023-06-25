7120aeed0c15553dafb23e163cf414adb52e1dfb
shark123YT <137716913+shark123YT@users.noreply.github.com>
2023-06-25 20:34:33 +0200

    Update TESTmod2.js

--- TESTmod2.js
+++ TESTmod2.js
@@ -1,10 +1,22 @@
 // Adding elements:
 elements.Cryotheum = {
-    color: "#b5f2f7",
+    color: "#57d2ff",
     behavior: behaviors.LIQUID,
     category: "liquids",
     viscosity: 10,
     state: "liquid",
     density: 1200,
     temp: -273,
+};
+elements.Basalz = {
+    color: "#240d04",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 10000000,
+    state: "liquid",
+    density: 2500,
+    temp: 3090,
+    reactions: {
+		"water": { elem1: "Cryotheum", elem2: "Baslaz" },
+	}
 };
