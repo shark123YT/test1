e85d7dc023c50b66902e568c7f68f98a1be36cee
shark123YT <137716913+shark123YT@users.noreply.github.com>
2023-06-25 20:27:10 +0200

    Add files via upload

---
+++ TESTmod2.js
@@ -0,0 +1,10 @@
+// Adding elements:
+elements.Cryotheum = {
+    color: "#133738",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 10,
+    state: "liquid",
+    density: 1200,
+    temp: -273,
+};
