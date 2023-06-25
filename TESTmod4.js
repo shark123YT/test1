96425087b976a097a097e4c8215ecabb1716fb48
shark123YT <137716913+shark123YT@users.noreply.github.com>
2023-06-25 20:48:36 +0200

    Add files via upload

---
+++ TESTmod4.js
@@ -0,0 +1,27 @@
+// Adding elements:
+elements.Cryotheum = {
+    color: "#57d2ff",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 10,
+    tempHigh: -83,
+    stateHigh: "null",
+    tempLow: -33400000,
+    stateLow: "ice",
+    state: "liquid",
+    density: 1200,
+    temp: -684,
+};
+elements.Basalz = {
+    color: "#240d04",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 1000,
+    tempHigh: 137800,
+    stateHigh: "plasma",
+    tempLow: 1250,
+    stateLow: "magma",
+    state: "liquid",
+    density: 2500,
+    temp: 3090,
+};
